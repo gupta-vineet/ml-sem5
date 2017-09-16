70d2ee1457ff0021443c26d817a65fca54aab12a
gupta-vineet <32011845+gupta-vineet@users.noreply.github.com>
2017-09-16 14:43:41 +0530

    Add files via upload

---
+++ FitnessFunction.py
@@ -0,0 +1,16 @@
+import math as m
+
+D=20
+LB=-100
+UB=100
+
+def FitnessFunction(x):
+    s=0
+    p=1
+    for i in range(0,D):
+        s=s+(x[i]**2/4000)
+        p=p*(m.cos(x[i]/m.sqrt(i+1)))
+        n=s-p+1
+    return n
+        
+            
