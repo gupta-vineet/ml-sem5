8c6590de594e8f6349c435a2c5c23ea5c9d62c8d
gupta-vineet <32011845+gupta-vineet@users.noreply.github.com>
2017-09-16 14:45:26 +0530

    Update FitnessFunction.py

--- FitnessFunction.py
+++ FitnessFunction.py
@@ -7,7 +7,7 @@ UB=100
 def FitnessFunction(x):
     s=0
     p=1
-    for i in range(0,D):
+    for i in range(0,D):                  #Griewank Function
         s=s+(x[i]**2/4000)
         p=p*(m.cos(x[i]/m.sqrt(i+1)))
         n=s-p+1
